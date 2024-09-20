f2ad347424c206a6c9576c1de41bf193f4ca8107
0layimika <110624406+0layimika@users.noreply.github.com>
2024-09-20 12:26:39 +0100

    Update settings.py

--- trustlink/settings.py
+++ trustlink/settings.py
@@ -111,19 +111,19 @@ WSGI_APPLICATION = 'trustlink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default= os.getenv("DB_URL"),
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default= os.getenv("DB_URL"),
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
